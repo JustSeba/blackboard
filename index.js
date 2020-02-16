e927dc6c524e1464a66fc96ae956bbff3467bc0d
JustSeba <59053604+JustSeba@users.noreply.github.com>
2020-02-16 20:59:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+$( ".monday" ).click(function() {
+   $(".column2").show();
+   $(".column3,.column4,.column5,.column6").hide();
+});
+
+$( ".tuesday" ).click(function() {
+   $(".column3").show();
+   $(".column2,.column4,.column5,.column6").hide();
+});
+
+$( ".wednesday" ).click(function() {
+   $(".column4").show();
+   $(".column2,.column3,.column5,.column6").hide();
+});
+
+$( ".thursday" ).click(function() {
+   $(".column5").show();
+   $(".column2,.column3,.column4,.column6").hide();
+});
+
+$( ".friday" ).click(function() {
+   $(".column6").show();
+   $(".column2,.column3,.column4,.column5").hide();
+});
+
+$('.row2 input').hover(
+  function() {
+    $(".row2 input, .row2 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row3 input').hover(
+  function() {
+    $(".row3 input, .row3 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row4 input').hover(
+  function() {
+    $(".row4 input, .row4 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row5 input').hover(
+  function() {
+    $(".row5 input, .row5 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row6 input').hover(
+  function() {
+    $(".row6 input, .row6 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row7 input').hover(
+  function() {
+    $(".row7 input, .row7 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row8 input').hover(
+  function() {
+    $(".row8 input, .row8 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row9 input').hover(
+  function() {
+    $(".row9 input, .row9 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row10 input').hover(
+  function() {
+    $(".row10 input, .row10 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row11 input').hover(
+  function() {
+    $(".row11 input, .row11 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row12 input').hover(
+  function() {
+    $(".row12 input, .row12 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row13 input').hover(
+  function() {
+    $(".row13 input, .row13 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row14 input').hover(
+  function() {
+    $(".row14 input, .row14 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row15 input').hover(
+  function() {
+    $(".row15 input, .row15 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row16 input').hover(
+  function() {
+    $(".row16 input, .row16 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row17 input').hover(
+  function() {
+    $(".row17 input, .row17 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row18 input').hover(
+  function() {
+    $(".row18 input, .row18 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
+
+$('.row19 input').hover(
+  function() {
+    $(".row19 input, .row19 td").toggleClass('hover2');
+    $(this).toggleClass('selectionGreen')
+  },
+)
